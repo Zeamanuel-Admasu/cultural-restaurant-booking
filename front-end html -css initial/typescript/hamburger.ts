f1fbd1f31e776fedd2e4541f8a4a65dcddb49ddd
SamuelT1995 <133096205+SamuelT1995@users.noreply.github.com>
2024-01-12 16:26:19 +0300

    Delete front-end html -css initial directory

--- front-end html -css initial/typescript/hamburger.ts
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelector('.nav-links') as HTMLElement;
-
-function showMenu(): void {
-    navLinks.style.right = "0";
-}
-
-function hideMenu(): void {
-    navLinks.style.right = "-200px";
-}
