4c20621da4fde06c3544c398dd0bf717239c069b
Isamahmedh <150470506+Isamahmedh@users.noreply.github.com>
2024-01-12 18:43:58 +0300

    auth-service

--- New folder/auth/auth.service.ts
+++ New folder/auth/auth.service.ts
@@ -33,5 +33,34 @@ export class AuthService {
 
 
     }
+    async login(email:string, password:string){
+        const retrievedUser = await this.userModel.findOne().where("email").equals(email);
+        if (!retrievedUser){
+            throw new BadRequestException("Email does not exist");
+        }
+        if(!await compare(password,retrievedUser.password)){
+            throw new BadRequestException("Wrong Password");
+        }
+        const payload = {
+            id: retrievedUser.id,
+        };
+        const jwt = await this.jwtService.signAsync(payload)
+        return jwt;
+    }
 
+    async getUser(cookie: string){
+        try{
+            const data = await this.jwtService.verifyAsync(cookie);
+            if (!data){
+                throw new UnauthorizedException();
+            }
+            const user = await this.userModel.findById(data.id);
+            return {status: "success",
+        id: user.id};
+
+        }
+        catch(err){
+            throw new UnauthorizedException();
+        }
+    }
 }
